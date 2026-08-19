e96bdcb58fcfed04bdd8c2b34afbbe4de25a3cd6
JoelDiaz93 <cdiaz_jl@hotmail.com.ar>
2026-08-19 11:17:04 -0500

    Update task

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,9 +1,125 @@
 'use strict';
 
+const { restoreNames } = require('./restoreNames');
+
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  it('should set firstName from fullName if firstName is undefined', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
 
-  it('should ', () => {});
+    restoreNames(users);
 
-  // write tests here
+    expect(users).toEqual([
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ]);
+  });
+
+  it('should set firstName from fullName if firstName key is missing', () => {
+    const users = [
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ]);
+  });
+
+  it('should not overwrite an existing firstName', () => {
+    const users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        fullName: 'Jonathan Doe',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        fullName: 'Jonathan Doe',
+      },
+    ]);
+  });
+
+  it('should not return anything', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+
+    const result = restoreNames(users);
+
+    expect(result).toBeUndefined();
+  });
+
+  it('should handle an empty array', () => {
+    const users = [];
+
+    restoreNames(users);
+
+    expect(users).toEqual([]);
+  });
+
+  it('should handle multiple users correctly', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+      {
+        firstName: 'Sarah',
+        lastName: 'Connor',
+        fullName: 'Sarah Connor',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+      {
+        firstName: 'Sarah',
+        lastName: 'Connor',
+        fullName: 'Sarah Connor',
+      },
+    ]);
+  });
 });
